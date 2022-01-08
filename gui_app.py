dcfbb7874e47fccf13219bee6fe9b799262efa8b
antonioliverjr <antoniobatistajr@gmail.com>
2022-01-08 14:29:24 -0300

    Logica de captura de dados e compactação do txt

--- gui_app.py
+++ gui_app.py
@@ -1,23 +1,29 @@
-from tkinter import *
+from tkinter import * 
+from tkinter import Tk, ttk
+from config import Color
 
 
-class GuiApp:
-    
-    def __init__(self) -> None:
-        self.janela = Tk()
-        self._title = ''
+class GuiApp():
+    def __init__(self):
+        self.main = Tk()
+        self.frame = ttk
+        self.mainframe = self.frame.Frame(self.main, padding="150 100", borderwidth=2
+        , relief=SOLID)
+        self.initialize_gui()
 
-    @property
-    def title(self):
-        return self._title
-
-    @title.setter
-    def title(self, titulo:str):
-        if not isinstance(titulo, (str)):
-            raise ValueError("Informe uma string!")
-        self._title = titulo
-
-    def iniciar(self, titulo:str):
-        self.title = titulo
-        self.janela.title(self._title)
-        return self.janela.mainloop()
+    def initialize_gui(self):
+        #self.main.geometry("500x500")
+        self.main.title("Robo Tkinter")
+        self.mainframe.grid(column=0, row=1, sticky=(N, W, E, S))
+        self.frame.Label(self.mainframe, text="Teste de label"
+        , background="black", foreground="white").grid(column=5, row=1, sticky=(W, E))
+        self.frame.Button(self.mainframe, text="Clique Aqui").grid(column=5, row=3, sticky=S)
+        
+        self.l = self.frame.Label(self.mainframe, text="Starting...")
+        self.l.grid()
+        self.l.bind('<Enter>', lambda e: self.l.configure(text='Moved mouse inside'))
+        self.l.bind('<Leave>', lambda e: self.l.configure(text='Moved mouse outside'))
+        self.l.bind('<ButtonPress-1>', lambda e: self.l.configure(text='Clicked left mouse button'))
+        self.l.bind('<3>', lambda e: self.l.configure(text='Clicked right mouse button'))
+        self.l.bind('<Double-1>', lambda e: self.l.configure(text='Double clicked'))
+        self.l.bind('<B3-Motion>', lambda e: self.l.configure(text='right button drag to %d,%d' % (e.x, e.y)))
--- manage.py
+++ manage.py
@@ -1,9 +1,30 @@
 from gui_app import GuiApp
+from commerce import Db_Commerce
+from adventure import Db_Adventure
 
 
 def main():
-    menu = GuiApp()
-    menu.iniciar(titulo="Robo Tkinter")
+    #menu = GuiApp()
+    #menu.main.mainloop()
+
+    adventure = Db_Adventure()
+    if adventure.consulta():
+        print("Arquivos selecionados!")
+    else:
+        print("Houve um erro ao selecionar os dados...")
+    
+
+
+    if adventure.exportar(adventure.tabela):
+        print("Arquivos exportados com sucesso!")
+        if adventure.verificar_arquivo():
+            print("Aguarde a compactação...")
+            if adventure.compactar():
+                print("Arquivo compactado com sucesso!")
+            else:
+                print("Houve um erro ao compactar...")
+
+    
 
 
 if __name__ == '__main__':
