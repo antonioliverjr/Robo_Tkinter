3f53bb1b52b6bd7861b84bdf2461428f015d39f5
antonioliverjr <antoniobatistajr@gmail.com>
2022-01-09 13:28:31 -0300

    Reescrevendo de forma correta, com o uso do Tkinter, criando os objetos da GUI

--- gui_app.py
+++ gui_app.py
@@ -6,24 +6,43 @@ from config import Color
 class GuiApp():
     def __init__(self):
         self.main = Tk()
-        self.frame = ttk
-        self.mainframe = self.frame.Frame(self.main, padding="150 100", borderwidth=2
-        , relief=SOLID)
-        self.initialize_gui()
-
-    def initialize_gui(self):
-        #self.main.geometry("500x500")
+        self.main['bg'] = Color.BACKGROUND_BG.value
+        self.main.geometry("550x330+200+200")
+        self.main.resizable(False, False)
+        self.main.iconbitmap(default="etl.ico")
         self.main.title("Robo Tkinter")
-        self.mainframe.grid(column=0, row=1, sticky=(N, W, E, S))
-        self.frame.Label(self.mainframe, text="Teste de label"
-        , background="black", foreground="white").grid(column=5, row=1, sticky=(W, E))
-        self.frame.Button(self.mainframe, text="Clique Aqui").grid(column=5, row=3, sticky=S)
+        self.mainframe = ttk.Frame(self.main, padding="3 3 12 12", borderwidth=2
+        , relief=SOLID)
+        self.style = ttk.Style()
+        self.style.theme_use('alt')
+        self.style.configure('TButton', background=Color.FONTE_TEXT.value, foreground=Color.BACKGROUND_IT.value)
+        self.style.map('TButton', foreground=[('active', Color.FONTE_TEXT.value)])
+
+        ''' Objetos da janela '''
+        self.titulo = ttk.Label(self.main, text="Importação de Produtos Adventure x Commerce",
+        background=Color.BACKGROUND_IT.value, foreground=Color.FONTE_TEXT.value, padding=5,
+        font="Times 16 bold", width=46, anchor=CENTER)
+        self.console = Listbox(self.main, background="white",
+         foreground=Color.FONTE_TEXT.value, font="Arial 12 italic", width=50)
+        self.barray = ttk.Scrollbar(self.main, orient=VERTICAL, command=self.console.yview)
+        self.console.configure(yscrollcommand=self.barray.set)
+        self.iniciar = ttk.Button(self.main, text="Iniciar")
+        self.__total_registros_label = ttk.Label(self.main, text="Total de Registros:", foreground=Color.FONTE_TEXT.value, 
+        background=Color.BACKGROUND_BG.value, anchor=CENTER)
+        self.total_registros = ttk.Label(self.main, background=Color.BACKGROUND_IT.value, anchor=CENTER)
+        self.__data_importacao_label = ttk.Label(self.main, text="Data Ultima Importação:", foreground=Color.FONTE_TEXT.value, 
+        background=Color.BACKGROUND_BG.value, anchor=CENTER)
+        self.data_importacao = ttk.Label(self.main, background=Color.BACKGROUND_IT.value, anchor=CENTER)
         
-        self.l = self.frame.Label(self.mainframe, text="Starting...")
-        self.l.grid()
-        self.l.bind('<Enter>', lambda e: self.l.configure(text='Moved mouse inside'))
-        self.l.bind('<Leave>', lambda e: self.l.configure(text='Moved mouse outside'))
-        self.l.bind('<ButtonPress-1>', lambda e: self.l.configure(text='Clicked left mouse button'))
-        self.l.bind('<3>', lambda e: self.l.configure(text='Clicked right mouse button'))
-        self.l.bind('<Double-1>', lambda e: self.l.configure(text='Double clicked'))
-        self.l.bind('<B3-Motion>', lambda e: self.l.configure(text='right button drag to %d,%d' % (e.x, e.y)))
+        ''' Distruição dos objetos na janela '''
+        self.titulo.grid(row=0, columnspan=6, sticky=(N, S, W, E), padx=10, pady=10)
+        self.console.grid(row=1, columnspan=5, sticky=(N,W), padx=10, pady=10)
+        self.barray.grid(row=1, column=5, pady=10, sticky=(N,S,W))
+        self.__total_registros_label.grid(row=2, column=0, ipadx=2, ipady=2, pady=3)
+        self.total_registros.grid(row=2, column=1, ipadx=3, ipady=3, pady=3)
+        self.__data_importacao_label.grid(row=2, column=2, ipadx=2, ipady=2, pady=3)
+        self.data_importacao.grid(row=2, column=3, ipadx=3, ipady=3, pady=3)
+        self.iniciar.grid(row=2, column=4, columnspan=2, padx=3, pady=3, ipadx=3,sticky=(N, S, W, E))
+
+
+
