328865b0705cfb21a64ae40ea6aceedea9df74b0
antonioliverjr <antoniobatistajr@gmail.com>
2022-01-09 13:29:29 -0300

    Organizando inicialização e fluxo das informação na GUi para o usuario

--- manage.py
+++ manage.py
@@ -1,41 +1,65 @@
-from gui_app import GuiApp
+from tkinter import StringVar
+from config import Commerce
+from gui_app import GuiApp, N, S, E, W
 from commerce import Db_Commerce
 from adventure import Db_Adventure
+from datetime import datetime
 
-
+''' Execução de JOB para extração e importação de dados '''
 def main():
-    #menu = GuiApp()
-    #menu.main.mainloop()
+    def data_log():
+        return datetime.now().strftime('%Y/%m/%d %H:%M:%S')
 
-    '''
-    Manipulação das classe Adventure e do GUI para adquirir os dados e gerar os arquivos txt e zip...
-    '''
-    adventure = Db_Adventure()
+    def start(add_result, registros, importacao):
+        '''
+        Manipulação das classe Adventure e do GUI para adquirir os dados e gerar os arquivos txt e zip...
+        '''
+        adventure = Db_Adventure()
 
-    if adventure.consulta():
-        print("Arquivos selecionados!")
-    else:
-        print("Houve um erro ao selecionar os dados...")
+        if adventure.consulta():
+            add_result(f'{data_log()}: Arquivos selecionados!')
+        else:
+            add_result(f'{data_log()}: Houve um erro ao selecionar os dados...')
 
-    if adventure.exportar(adventure.tabela):
-        print("Arquivos exportados com sucesso!")
-        if adventure.verificar_arquivo():
-            print("Aguarde a compactação...")
-            if adventure.compactar():
-                print("Arquivo compactado com sucesso!")
-            else:
-                print("Houve um erro ao compactar...")
+        if adventure.exportar(adventure.tabela):
+            add_result(f'{data_log()}: Arquivos exportados com sucesso!')
+            if adventure.verificar_arquivo():
+                add_result(f'{data_log()}: Aguarde a compactação...')
+                if adventure.compactar():
+                    add_result(f'{data_log()}: Arquivo compactado com sucesso!')
+                else:
+                    add_result(f'{data_log}: Houve um erro ao compactar...')
 
-    '''
-    Manipulação das classe Commerce e do GUI para copiar o arquivo zip e inserir os dados do txt...
-    '''
+        '''
+        Manipulação das classe Commerce e do GUI para copiar o arquivo zip e inserir os dados do txt...
+        '''
+        commerce = Db_Commerce()
+        commerce.copy_arquivos()
+        importados = commerce.importar()
+        if importados > 0:
+            add_result(f'{data_log()}: Foram importados {str(importados)} Registro(s).')
+            registros.set(f'{str(importados)}')
+            importacao.set(f'{data_log()}')
 
-    commerce = Db_Commerce()
-    commerce.copy_arquivos()
-    result = commerce.importar()
-    if result > 0:
-        print(f'Foram importados {result} Registro(s).')
 
+    menu = GuiApp()
+    result = []
+    resultados = StringVar(value=result)
+
+    def add_result(texto):
+       result.append(texto)
+       resultados.set(result)
+
+    add_result("Console de Operações")
+    add_result(f"{data_log()}: Aguardando Iniciar...")
+
+    registros = StringVar()
+    importacao = StringVar()
+    menu.total_registros['textvariable'] = registros
+    menu.data_importacao['textvariable'] = importacao
+    menu.iniciar['command'] = lambda: start(add_result, registros, importacao)
+    menu.console['listvariable'] = resultados
+    menu.main.mainloop()
 
 if __name__ == '__main__':
     main()
